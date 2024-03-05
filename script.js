f5ff315f9694cbe37c1104f35bed990fc6cdc298
joilslv <joilsonsilvasky@gmail.com>
2024-03-05 02:54:04 -0300

    Ajustes no Java Script

--- script.js
+++ script.js
@@ -2,31 +2,56 @@
 
 
 const convertButton = document.querySelector(".convert-button")
-const currencySelectTo = document.querySelector(".select-to-convert")
-const currencySelect = document.querySelector(".select-converted")
+
+
 
 function convertValues() {
     const inputValue = document.querySelector(".input-value").value
     const showsValueToConvert = document.querySelector(".valor")
     const showsValueConverted = document.querySelector(".valor-convertido")
-    console.log(showsValueConverted.value)
+    const currencySelect = document.querySelector(".select").value
+    const currencySelectTo = document.querySelector(".select-to").value
+    const paragrafoEuro = document.querySelector(".europ")
 
     const euroToday = 5.37
-    const dolarToday = 4.95
-
+    const dolarToday = 4.9
+    const convertedValue = inputValue/euroToday
 
     showsValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
-        currency: "BRL" }).format (inputValue)
+        currency: "BRL" }).format(inputValue)
+    /*showsValueConverted.innerHTML = new Intl.NumberFormat("ESP", {
+        style: "currency",
+        currency: "EUR" }).format(convertedValue)
+    console.log(currencySelectTo)*/
+
+
+
     
 
-    if (currencySelect == "euro") {
+    if (currencySelectTo == "euro") {
         showsValueConverted.innerHTML = new Intl.NumberFormat("ESP", {
             style: "currency",
             currency: "EUR"
         }).format(inputValue / euroToday)
     }
 
+    if (currencySelectTo == "dolar") {
+        showsValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValue / dolarToday)
+    }
+
+    if (currencySelectTo == "real") {
+        showsValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputValue / 1)
+        
+    }
+
+   
 
 }
 
